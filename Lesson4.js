80b6e6984dee100036b5389e0f96d7dbd781d611
Krulyk Andrii <andreykrulik@gmail.com>
2019-03-15 19:47:42 +0200

    classwork

---
+++ Lesson4.js
@@ -0,0 +1,9 @@
+"use strict";
+var num1, num2, result;
+
+num1 = +prompt('enter the first number', 0);
+num2 = +prompt('enter the second number', 0);
+
+result = num1 + num2;
+
+alert('Result of addition = ' + result);
---
+++ Lesson6Obj.js
@@ -0,0 +1,12 @@
+"use strict";
+// let name = {};
+// user.name = "Vasya";
+// user.surname ="petrov";
+// user.name = "sergey";
+// delete user.name;
+// console.log(user);
+let sum = 0;
+for (let name in salaries){
+    sum += salaries(name);
+}
+console.log(name);
