4f0c237260ff72adf13ff38be21f001d322924c1
Krulyk Andrii <andreykrulik@gmail.com>
2019-03-22 19:06:32 +0200

    Classromissue

--- Additionalprogram1.js
+++ Additionalprogram1.js
@@ -1,27 +1,27 @@
 "use strict";
-// var table, row, cell, i, j;
-// table = document.createElement('table');
-// table.style.textAlign = 'right';
-// table.style.fontFamily = 'monospace';
-// for (i=1; i<10; i++) {
-//     row = document.createElement('tr');
-//     for (j=1; j<10; j++) {
-//         cell = document.createElement(i == 1 || j == 1 ? 'th' : 'td');
-//         cell.appendChild(document.createTextNode(i*j));
-//         cell.style.padding = '4px';
-//         cell.style.width = 100 / 9 + '%';
-//         row.appendChild(cell);
-//     }
-//     table.appendChild(row);
-// }
-// document.body.appendChild(table);
-
-
-
-for (let i = 1;i<10;i++){
-    for (let y = 1;y<10;y++){
-        document.write(i + "*" + y + "=" + (i * y) + "<br>");
+var table, rows, cells, i, j;
+table = document.createElement('table');
+table.style.textAlign = 'right';
+table.style.fontFamily = 'monospace';
+for (i=1; i<10; i++) {
+    rows = document.createElement('tr');
+    for (j=1; j<10; j++) {
+        cells = document.createElement(i === 1 || j === 1 ? 'th' : 'td');
+        cells.appendChild(document.createTextNode(i*j));
+        cells.style.padding = '4px';
+        cells.style.width = 100 / 9 + '%';
+        rows.appendChild(cells);
     }
-    document.write("<br>");
+    table.appendChild(rows);
 }
+document.body.appendChild(table);
+
+
+
+// for (let i = 1;i<10;i++){
+//     for (let y = 1;y<10;y++){
+//         document.write(i + "*" + y + "=" + (i * y) + "<br>");
+//     }
+//     document.write("<br>");
+// }
 
